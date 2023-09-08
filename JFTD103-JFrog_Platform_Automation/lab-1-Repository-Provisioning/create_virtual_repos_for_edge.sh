1b6de464c4f854eeece37440449ac82b0275e7f6
Maheshkumar Gopalakrishnan <35077604+maheshjfrog@users.noreply.github.com>
2023-09-08 10:31:51 -0700

    Fix for edge virtial repo creation (#51)

--- JFTD103-JFrog_Platform_Automation/lab-1-Repository-Provisioning/create_virtual_repos_for_edge.sh
+++ JFTD103-JFrog_Platform_Automation/lab-1-Repository-Provisioning/create_virtual_repos_for_edge.sh
@@ -3,5 +3,5 @@ for row in $(cat ./virtual-repos-for-edge.json | jq -r '.[] | @base64'); do
       echo ${row} | base64 --decode | jq -r ${1}
     }
 
-    jf rt repo-create template-virtual-rescue.json --vars "repo-name=$(_jq '.key');package-type=$(_jq '.packageType');repo-type=$(_jq '.rclass');repo-layout=$(_jq '.repoLayoutRef');external-remote-repo-name=$(_jq '.externalDependenciesRemoteRepo');repos=$(_jq '.repositories')"
+    jf rt repo-create template-virtual-rescue.json --vars "repo-name=$(_jq '.key');package-type=$(_jq '.packageType');repo-type=$(_jq '.rclass');repo-layout=$(_jq '.repoLayoutRef');external-remote-repo-name=$(_jq '.externalDependenciesRemoteRepo');repos=$(_jq '.repositories');env=$(_jq '.environments')"
 done
